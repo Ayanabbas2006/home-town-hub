8c88031d6a8098c4c277cef725f572ce8b9e9815
Ayanabbas2006 <mojisshuja@gmail.com>
2026-03-18 21:31:27 +0530

    backend update

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,24 @@ import models, schemas, crud, Security
 from database.database import get_db, engine
 from upload import upload_photo
 from database import database
+from routers.send_mail import *
+from fastapi.middleware.cors import CORSMiddleware
+
 database.Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="Hometown Hub Api", version="0.0.1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=['*'],
+    allow_method=['*'],
+    allow_headers=['*']
+)
 
 @app.get("/")
 def root():
     return {"message": "Hometown Hub API is running ✅"}
+
+@app.get("/{name}/{email}")
+def email(name,email):
+    send_otp(name=name,receiver_mail=email)
---
+++ backend/routers/send_mail.py
@@ -0,0 +1,39 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+def generate_otp():
+    return random.randint(111111,999999)
+
+def send_otp(name,receiver_mail):
+    otp=generate_otp()
+    message = Mail(
+        from_email='mojisshuja@gmail.com',
+        to_emails=receiver_mail,
+        subject='Thanks for registering at HomeTownHub',
+        html_content=f"""
+        <h1> HomeTownHub</h1>
+        Dear {name},
+        <div>
+        We are pleased by your presence, here is your 6-digit secret code.
+        kindly DO NOT SHARE IT WITH ANYONE!
+        <div>
+        <h4>OTP: {otp}</h4>
+        </div>
+        </div>
+        Thanks,<br>
+        Team<br>
+        HomeTownHub
+        """
+    )
+
+    try:
+        sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+        response = sg.send(message=message)
+        return (response.status_code)
+    except Exception  as error:
+        return (error)
+print(send_otp("Ayan",'ilovenothing007@gmail.com'))
