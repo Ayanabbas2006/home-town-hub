9ad05720d598b585af32afc7d951cf895b84e0b1
Ayanabbas2006 <mojisshuja@gmail.com>
2026-03-21 14:07:27 +0530

    new

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from datetime import timedelta
 import models, schemas, crud, Security
 from database.database import get_db, engine
-from backend.routers.upload import upload_photo
+from backend.routers.upload import upload_router
 from database import database
 from routers.mail import *
 from fastapi.middleware.cors import CORSMiddleware
@@ -11,7 +11,7 @@ from fastapi.middleware.cors import CORSMiddleware
 database.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Hometown Hub Api", version="0.0.1")
-app.include_router(router=router)
+app.include_router(router=[router,upload_router])
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://hometownhub.vercel.app","http:localhost:5500"],
