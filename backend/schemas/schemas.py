5b3faaea61ba1b58acaf9dc84856c3cd45634705
Ayanabbas2006 <mojisshuja@gmail.com>
2026-03-22 20:01:54 +0530

    update

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -6,14 +6,13 @@ class UserCreate(BaseModel):
     full_name: str
     email: EmailStr
     hometown: str
-    password: str
+    hashed_pw: str
     photo_url: str
 
 class UserOut(BaseModel):
     Id: int
     full_name: str
     email: str
-    hashed_pw: str
     created_at: datetime
     photo_url: Optional[str] = None
     class config:
